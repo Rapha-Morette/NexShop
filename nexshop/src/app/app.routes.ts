2a5d8b93abb5d9846973a0fde09935d95d887c40
Rapha-Morette <raphaelmrtt14@gmail.com>
2025-06-11 20:38:09 -0300

    Finalizado tela de login e criação de usuários com o Banco de dados funcional

--- nexshop/src/app/app.routes.ts
+++ nexshop/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login'
+import { Login } from './pages/login/login';
+import { Cadastro } from './pages/cadastro/cadastro';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: Login }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  {
+    path: 'cadastro',
+    loadComponent: () =>
+      import('./pages/cadastro/cadastro').then(
+        (m) => m.Cadastro
+      ),
+  },
 ];
--- nexshop/src/app/pages/login/login.ts
+++ nexshop/src/app/pages/login/login.ts
@@ -1,25 +1,37 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { UsuarioService } from '../../services/usuario';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrls: ['./login.scss']
+  styleUrls: ['./login.scss'],
 })
 export class Login {
   username = '';
   password = '';
   errorMessage = '';
 
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
+
   onSubmit() {
-    if (this.username === 'admin' && this.password === '1234') {
-      console.log('Login bem-sucedido!');
-      // Aqui futuramente vai a validação de IP/MFA/biometria
-    } else {
-      this.errorMessage = 'Usuário ou senha inválidos.';
-    }
+    this.usuarioService
+      .login(this.username, this.password)
+      .subscribe((usuarios) => {
+        if (usuarios.length > 0) {
+          console.log('Login bem-sucedido:', usuarios[0]);
+          this.router.navigate(['/home']);
+        } else {
+          this.errorMessage = 'Credenciais inválidas';
+        }
+      });
+  }
+
+  irParaCadastro() {
+    this.router.navigate(['/cadastro']);
   }
 }
